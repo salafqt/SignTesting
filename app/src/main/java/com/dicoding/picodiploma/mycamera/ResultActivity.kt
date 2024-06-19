cae74da799f0f3ccc5ab27bca4850814a8e1a6d1
Salman Alfarisi <salafqt@gmail.com>
2024-06-19 20:14:36 +0700

    commiting

---
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/ResultActivity.kt
@@ -0,0 +1,37 @@
+package com.dicoding.picodiploma.mycamera
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.picodiploma.mycamera.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imageUri = Uri.parse(intent.getStringExtra(EXTRA_IMAGE_URI))
+        val classificationResult = intent.getStringExtra(EXTRA_RESULT)
+
+        imageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.resultImage.setImageURI(it)
+        }
+
+        classificationResult?.let {
+            Log.d("Classification Result", "showResult: $it")
+            binding.resultText.text = it
+        }
+    }
+
+    companion object {
+        const val EXTRA_IMAGE_URI = "extra_image_uri"
+        const val EXTRA_RESULT = "extra_result"
+    }
+}
